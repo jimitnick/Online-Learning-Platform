dde1b5a9c1d9fa59fa485e973cc5425b916049fe
Abhijith <jithumitnick@gmail.com>
2025-03-13 21:54:49 +0530

    Updated the profile section so that the data of the user if updated also remains the same even after reloading or moving away to some other page

--- dashboard/Profile/scriptProfile.js
+++ dashboard/Profile/scriptProfile.js
@@ -5,12 +5,28 @@ document.querySelectorAll(".Email").forEach(elem =>{
     elem.textContent = localStorage.getItem("Current User Email");
 })
 
+let Dets = JSON.parse(localStorage.getItem("userProfileDets"));
+[Dets].forEach((dets)=>{
+    document.querySelector("#phone").textContent = dets.mobile;
+    document.querySelector("#location").textContent = dets.Location;
+    document.querySelector("#bio").textContent = dets.Bio;
+})
+
 document.querySelector("#edit-profile").addEventListener('click',()=>{
-    var phone = confirm("Do you want to change the phone number ?") ? prompt("Enter the new phone number : ") : "";
-    var location = confirm("Do you want to change the location ?") ? prompt("Enter the new location : ") : "";
-    var bio = confirm("Do you want to change the bio ?") ? prompt("Enter the new bio : ") : "";
+    var phone = confirm("Do you want to change the phone number ?") ? prompt("Enter the new phone number : ") : "xxxx-xxx-xxx";
+    var location = confirm("Do you want to change the location ?") ? prompt("Enter the new location : ") : "Add Your Location";
+    var bio = confirm("Do you want to change the bio ?") ? prompt("Enter the new bio : ") : "Add Bio";
 
     document.querySelector("#phone").textContent = phone;
     document.querySelector("#location").textContent = location;
     document.querySelector("#bio").textContent = bio;
+
+    let det1 = new Array();
+    det1 = {
+        mobile : phone,
+        Location : location,
+        Bio : bio   
+    }
+    localStorage.setItem("userProfileDets",JSON.stringify(det1));
 })
+
